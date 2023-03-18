3f893cc699a368e492582adf1f1d5924db984504
narahsouza <narahnsa@gmail.com>
2023-03-18 11:17:01 -0300

    Update

---
+++ Sololearn_old/Javascript/Practice05_2.js
@@ -0,0 +1,2 @@
+//fix the code
+console.log('I\'m learning JavaScript');
---
+++ Sololearn_old/Javascript/Practice05_3.js
@@ -0,0 +1,2 @@
+console.log('I\'m learning JavaScript');
+console.log("A quote: \"Java is to JavaScript as ham is to hamster.\" ");
